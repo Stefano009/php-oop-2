feee5863b77f80b2a28119b57f5458cc7ba48a46
Stefano009 <stefano.funaro@gmail.com>
2021-10-22 12:40:29 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- all my links -->
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>php-oop-2</title>
+    <?php require_once ('./classes/require.php') ?>
+</head>
+<body>
+ 
+<!-- calling my js file -->
+<script src="./js/main.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,5 @@
+Vue.config.devtools = true;
+
+const App = new Vue ({
+    el: '#root',
+})
